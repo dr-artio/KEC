6a946ae4a92f336152d817f4bd0f0d4cec94eb91
aartsiomenka1 <artyomenkoav@gmail.com>
2013-10-10 12:54:59 -0400

    Maven commit

--- src/errorcorrection/HaplComp.java
+++
@@ -1,9 +0,0 @@
-package errorcorrection;
-
-import java.util.Comparator;
-
-public class HaplComp implements Comparator<Haplotype>{
-		public int compare(Haplotype r1, Haplotype r2) {
-			return -(r1.frequency - r2.frequency);
-		}
-}
